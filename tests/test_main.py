669476eea67b02c9cf4447b1d8cf9e5e4d39db9d
RomesAll <romesky845@gmail.com>
2025-11-07 17:15:52 +0300

    Параметризация тестов

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
-from src.main import A
+from src.main import Calculator
+import pytest
 
-def test_sum_1():
-    x = 1
-    y = 2
-    assert x + y == 3
+@pytest.mark.parametrize("x, y, res", 
+                         [(1, 2, 0.5), (4, 2, 2), (5, -1, -5)])
+def test_divide(x, y, res):
+    assert Calculator().divide(x, y) == res
 
-def test_sum_2():
-    x = 5
-    y = 2
-    assert x + y == 7
+@pytest.mark.parametrize("x, y, res", 
+                         [(1, 2, 3), (4, 2, 6), (5, -1, 4)])
+def test_sum(x, y, res):
+    assert Calculator().add(x, y) == res
